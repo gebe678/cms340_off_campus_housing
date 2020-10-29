02dfc8d685d0dbd853966ea7ccee9baed28f762a
gebe678 <lehrerg6@gmail.com>
2020-10-29 11:59:31 -0400

    added database connection capabilities

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Jacob, Maria, Griffin
 
 Narrative: This project will help students living at Rollins make informed decisions about off campus housing. It will show housing options near rollins and help students budget for all expenses accociated with living on their own. 
+
+Password For Database: Griffin&Jacob&Maria!
--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 // Variables reqired for running the express server
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const path = require("path");
 const app = express();
+
+app.use(bodyParser.json());
 const port = 8080;
 
 // Variables required for the mysql database
 const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "test"
+});
+
+connection.connect(function(err){
+    if(err)
+    {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+
+    console.log("connected as id " + connection.threadId);
+});
 
 
 
