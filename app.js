ea10f204de973e434d0565e14cd8f3e9438b9800
gebe678 <lehrerg6@gmail.com>
2020-10-29 00:23:49 -0400

    added mysql server connection capabilities

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
+// Variables reqired for running the express server
 const express = require("express");
 const path = require("path");
 const app = express();
 const port = 8080;
+
+// Variables required for the mysql database
+const mysql = require("mysql");
+
+
 
 app.use(express.static(__dirname + "/assets"));
 
