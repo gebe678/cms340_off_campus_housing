0f516b76bcf283bfae3dbcdfc8a46ca3d3dd5e4c
gebe678 <lehrerg6@gmail.com>
2020-10-28 23:35:22 -0400

    added some starter code for the website

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => res.send("hello world by Maira!"));
+app.use(express.static(__dirname + "/assets"));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/index.html')));
 
 app.listen(port, () => console.log(`Server is listening on port ${port}!`));
