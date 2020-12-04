cacd4249808944441970bcc4586620d0975f4b4d
gebe678 <lehrerg6@gmail.com>
2020-12-04 15:56:15 -0500

    added form parser express module

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "test",
+    database: "housing_information",
     port: 3306
 });
 
